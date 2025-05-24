509aaf7e951bc7afbfe512e927ab39b01505e911
DecksDark <patrickdiazgrandez@gmail.com>
2025-05-24 18:04:43 +0200

    fixed missing degradeSignal in transmit

--- Cable.java
+++ Cable.java
@@ -9,13 +9,14 @@ public class Cable extends SignalCarrier{
         if (s == null)
             r = new Result(false, "No se encontro ninguna señal");
         else{
+            degradeSignal();
             super.getNextComp().setSignal(s);
             r = new Result(true, "Señal enviada al siguiente componente");
 		}
         return r;
     }
 
-    private void degradeSignal(Signal s){
+    private void degradeSignal(){
 		for (int i = 1; i<=length; i++){
 			super.getSignal().decreaseStrength(signalDegrad);
 		}
